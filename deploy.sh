26b0ef0293f52e2b0d371e0577705cc1ca9ff2a8
Dmytro005 <harazdovskiy@gmail.com>
2020-01-03 14:31:10 +0200

    Fixed config 5

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 ssh -i ./deploy_key TravisCIDeployKey@35.184.79.30 <<-"ENDSSH"
-cd /home/dmytro/express-microservices
-sudo git pull
-sudo systemctl start docker
-sudo docker-compose -f ./docker-compose.prod.yml up -d --build
+    cd /home/dmytro/express-microservices
+    sudo git pull
+    sudo systemctl start docker
+    sudo docker-compose -f ./docker-compose.prod.yml up -d --build
 ENDSSH
