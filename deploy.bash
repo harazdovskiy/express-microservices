1369b129271902976d302ceefc399be4532154c4
Dmytro005 <harazdovskiy@gmail.com>
2020-01-03 14:27:49 +0200

    Fixed config 3

--- deploy.bash
+++ deploy.bash
@@ -1,4 +1,4 @@
-ssh -i ./deploy_key TravisCIDeployKey@35.184.79.30 <<-'ENDSSH'
+ssh -i ./deploy_key TravisCIDeployKey@35.184.79.30 <<-"ENDSSH"
 cd /home/dmytro/express-microservices
 sudo git pull
 sudo systemctl start docker
