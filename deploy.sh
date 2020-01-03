5d881110e9076e49178a30ccbdd3b4e8df9015c6
Dmytro005 <harazdovskiy@gmail.com>
2020-01-03 15:09:03 +0200

    Setuped basic travis ci deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
-# ssh -i ./deploy_key TravisCIDeployKey@35.184.79.30 <<-"ENDSSH"
-    cd /home/dmytro/express-microservices
-    sudo git pull
-    sudo systemctl start docker
-    sudo docker-compose -f ./docker-compose.prod.yml up -d --build
-# ENDSSH
+cd /home/dmytro/express-microservices
+sudo git pull
+sudo systemctl start docker
+sudo docker-compose -f ./docker-compose.prod.yml up -d --build
