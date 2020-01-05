987d478d629f5af173a8642c191325a2e67cc15d
Dmytro005 <harazdovskiy@gmail.com>
2020-01-05 17:47:50 +0200

    Extended deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 cd /home/dmytro/express-microservices
 sudo git pull
 sudo systemctl start docker
-sudo docker-compose -f ./docker-compose.prod.yml up -d --build
+sudo docker-compose -f ./docker-compose.prod.yml build --no-cache
+sudo docker-compose -f ./docker-compose.prod.yml up -d --force-recreate
