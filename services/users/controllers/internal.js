f82791cbb824dbc9920e762d3069e76b85644891
Dmytro005 <harazdovskiy@gmail.com>
2020-01-05 13:59:39 +0200

    Internal route tests out of docker

--- services/users/controllers/internal.js
+++ services/users/controllers/internal.js
@@ -22,6 +22,7 @@ router.get("/:id", async (req, res) => {
   try {
     return res.send({
       err: false,
+      some: "shit",
       data: await UsersService.getUser(req.params.id)
     });
   } catch (error) {
